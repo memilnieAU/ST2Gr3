a226d515daa9b1e184de1c988596a157eb6fc154
Trine Kæstel Madsen <au615584@uni.au.dk>
2020-04-20 09:26:37 +0200

    oprettet login-vindue og lagt komponenter ind

---
+++ Brugergrænseflade/p_layer/loginView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace p_layer
+{
+    /// <summary>
+    /// Interaction logic for loginView.xaml
+    /// </summary>
+    public partial class loginView : Window
+    {
+        public loginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
