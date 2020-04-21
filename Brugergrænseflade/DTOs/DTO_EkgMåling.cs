f9012d2ac60056e4d14cb18b643505eb6d71f0f6
MemilnieAU <201908775@post.au.dk>
2020-04-21 21:18:54 +0200

    Test på at hente fra en bestemt fil Test på at convetere det til et DTo objekt

--- Brugergrænseflade/DTOs/DTO_EkgMåling.cs
+++ Brugergrænseflade/DTOs/DTO_EkgMåling.cs
@@ -8,5 +8,26 @@ namespace DTOs
 {
     public class DTO_EkgMåling
     {
+        static int ekgTestIdStatic = 1;
+        int ekgTestId = 0;
+        int størelse;
+        DateTime tid;
+
+        Dictionary<DateTime, int> målepunkter;
+
+        public DTO_EkgMåling()
+        {
+            ekgTestIdStatic++;
+            ekgTestId = ekgTestIdStatic;
+
+            målepunkter = new Dictionary<DateTime, int>();
+        }
+
+        public void Tilføjmålepunkt(DateTime tid, int størelse)
+        {
+            målepunkter.Add(tid, størelse);
+
+        }
+
     }
 }
--- Brugergrænseflade/l_layer/EKG_HentDummyData.cs
+++ Brugergrænseflade/l_layer/EKG_HentDummyData.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DTOs;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,5 +16,55 @@ namespace l_layer
     /// </summary>
     class EKG_HentDummyData
     {
+        List<DTO_EkgMåling> AllSampels;
+        public EKG_HentDummyData()
+        {
+            AllSampels = new List<DTO_EkgMåling>();
+        }
+
+
+        void HentFraCsvFil()
+        {
+            AllSampels.Add(new DTO_EkgMåling());
+            int sidsteTilføjet = AllSampels.Count - 1;
+
+
+            // string-objekter til at gemme det som læses fra filen
+            string inputRecord;
+            string[] inputFields;
+
+
+            // opret de nødvendige stream-objekter
+            FileStream input = new FileStream("Test_Atrieflimmer_1.txt", FileMode.Open, FileAccess.Read);
+            StreamReader fileReader = new StreamReader(input);
+
+
+            // indlæs sålænge der er data i filen
+            while ((inputRecord = fileReader.ReadLine()) != null)
+            {
+                // split data op i fornavn, efternavn og telefonnummer
+                inputFields = inputRecord.Split(';');
+
+                // gem data i listen
+                if (Convert.ToDateTime(inputFields[0]) != null)
+                {
+                    AllSampels[sidsteTilføjet].Tilføjmålepunkt(Convert.ToDateTime(inputFields[0]), Convert.ToInt32(inputFields[1]));
+                }
+            }
+
+            // luk adgang til filen
+            fileReader.Close();
+
+            // udskriv de indlæste data på skærmen
+            //Console.WriteLine("Disse kontakter blev indlæst:\n");
+
+            //foreach (Person p in contacts)
+            //{
+            //    Console.WriteLine(p);
+            //}
+
+            //Console.WriteLine();
+        }
+
     }
 }
