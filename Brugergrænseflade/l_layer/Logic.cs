269b49608027271e8ab388d1306317510337c688
MarieLouiseBruun <au615252@uni.au.dk>
2020-04-21 20:48:35 +0200

    starter nu i LoginWindow i stedet for mainWindow. LoginWindow er også sat lidt pænere op

---
+++ Brugergrænseflade/l_layer/Logic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace l_layer
+{
+    public class Logic
+    {
+        public Logic()
+        { }
+        public bool checkLogin(String socSecNb, String pw)
+        {
+            return (socSecNb == "999999-0000" && pw == "testpw");
+        }
+    }
+}
