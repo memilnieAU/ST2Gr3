2e0153ee43fb9516b813f80f393dbcc4cef345a7
MemilnieAU <201908775@post.au.dk>
2020-04-21 17:11:01 +0200

    Start på at analyse delen Der er oprettet Clase Libary til diverse DTO objekter og til logik laget
    Der er lavet en DummyClass til at kunne hente data direkte fra en sted på ens egen computer - Det skal selvfølgelig være fra en database på sigt

---
+++ Brugergrænseflade/l_layer/EKG_HentDummyData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace l_layer
+{
+    /// <summary>
+    /// Denne klasse er en logic stump
+    /// Denne skal hente data som bruges til at teste "EKG_Analyse" med.
+    /// På sigt skal den skal dataen komme fra en database (Lokal/Offentlig)
+    /// 
+    /// </summary>
+    class EKG_HentDummyData
+    {
+    }
+}
