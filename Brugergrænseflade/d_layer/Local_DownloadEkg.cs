121e36ee920ca15f301f54fc526fdbfe30afa937
MemilnieAU <201908775@post.au.dk>
2020-05-01 16:07:36 +0200

    En ny SQLQuery

--- Brugergrænseflade/d_layer/Local_DownloadEkg.cs
+++ Brugergrænseflade/d_layer/Local_DownloadEkg.cs
@@ -4,11 +4,24 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DTOs;
 
 namespace d_layer
 {
     public class Local_DownloadEkg
     {
+        public int id_måling { get; set; }
+        public string id_medarbejder { get; set; }
+        public string borger_cprnr { get; set; }
+        public DateTime start_tidspunkt { get; set; }
+        public double[] raa_maalepunkter { get; set; }
+        public double samplerate_hz { get; set; }
+        DTO_EkgMåling målingFraDB;
+
+        public Local_DownloadEkg()
+        {
+
+        }
         public double[] hentEkgData(int index)
         {
             SqlConnection conn;
@@ -25,12 +38,15 @@ namespace d_layer
                 rdr = cmd.ExecuteReader();
                 if (rdr.Read())
                     bytesArr = (byte[])rdr["raa_data"];
+
+
                 tal = new double[bytesArr.Length / 8];
 
                 for (int i = 0, j = 0; i < bytesArr.Length; i += 8, j++)
                     tal[j] = BitConverter.ToDouble(bytesArr, i);
             }
             conn.Close();
+
             return tal;
         }
         public int hentAntalletAfMålinger()
@@ -54,6 +70,49 @@ namespace d_layer
             }
             conn.Close();
             return tal;
+        }
+
+        public DTO_EkgMåling hentMåling(int id_måling)
+        {
+
+            målingFraDB = new DTO_EkgMåling(id_måling, hentEkgData(id_måling));
+            SqlConnection conn;
+            const String db = "F20ST2ITS2201908775";
+
+            conn = new SqlConnection("Data Source = st-i4dab.uni.au.dk;Initial Catalog = " + db + ";Persist Security Info = True;User ID = " + db + ";Password = " + db + "");
+            conn.Open();
+            SqlDataReader rdr;
+
+            string selectString = "Select * from SP_NyeEkger where id_måling = " + id_måling;
+            using (SqlCommand cmd = new SqlCommand(selectString, conn))
+            {
+                rdr = cmd.ExecuteReader();
+                if (rdr.Read())
+                {
+                    if (rdr["id_medarbejder"] != DBNull.Value)
+                        målingFraDB.id_medarbejder = (string)rdr["id_medarbejder"];
+
+
+                    if (rdr["borger_cprnr"] != DBNull.Value)
+                        målingFraDB.borger_cprnr = (string)rdr["borger_cprnr"];
+
+                    if (rdr["start_tidspunkt"] != DBNull.Value)
+                        målingFraDB.start_tidspunkt = (DateTime)rdr["start_tidspunkt"];
+                    if (rdr["antal_maalepunkter"] != DBNull.Value)
+                        målingFraDB.antal_maalepunkter = (int)rdr["antal_maalepunkter"];
+                    if (rdr["samplerate_hz"] != DBNull.Value)
+                        målingFraDB.samplerate_hz = (double)rdr["samplerate_hz"];
+
+
+                }
+
+
+            }
+            conn.Close();
+
+
+
+            return målingFraDB;
         }
 
     }
